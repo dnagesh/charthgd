7a154b05f6a16274a20b459055bf1f81635011ca
Nagesh Darjipati <nagesh.darjipati@gmail.com>
2025-10-20 09:56:47 +0100

    path-20-oct-1

--- src/main/java/com/smartsourcing/charitycommission/rsi/model/entity/Submission.java
+++ src/main/java/com/smartsourcing/charitycommission/rsi/model/entity/Submission.java
@@ -1,4 +1,4 @@
-package uk.gov.ccew.rsi.model.entity;
+package com.smartsourcing.charitycommission.rsi.model.entity;
 
 import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
--- src/main/java/com/smartsourcing/charitycommission/rsi/repository/SubmissionRepository.java
+++ src/main/java/com/smartsourcing/charitycommission/rsi/repository/SubmissionRepository.java
@@ -2,7 +2,7 @@ package com.smartsourcing.charitycommission.rsi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import uk.gov.ccew.rsi.model.entity.Submission;
+import com.smartsourcing.charitycommission.rsi.model.entity.Submission;
 
 @Repository
 public interface SubmissionRepository extends JpaRepository <Submission, Long> {
