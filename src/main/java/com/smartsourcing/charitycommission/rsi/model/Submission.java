9ae2feff2cea7162f704dfdf99716e911f7b9c0f
dnagesh <dnagesh@users.noreply.github.com>
2025-10-20 09:53:31 +0100

    Add files via upload

---
+++ src/main/java/com/smartsourcing/charitycommission/rsi/model/Submission.java
@@ -0,0 +1,26 @@
+package uk.gov.ccew.rsi.model.entity;
+
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+
+import java.util.Map;
+
+@Entity
+@Getter
+@Setter
+public class Submission {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Type(JsonType.class)
+    @Column(columnDefinition = "JSON")
+    private Map<String, Object> inputData;
+}
