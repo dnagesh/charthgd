424f9bd6be9e4c5c5b8bfbd0456a3115a7d7749a
dnagesh <dnagesh@users.noreply.github.com>
2026-01-07 18:48:45 +0000

    Add files via upload

---
+++ ErrorSummary.java
@@ -0,0 +1,22 @@
+package uk.gov.ccew.rsi.validation.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorSummary {
+
+    /**
+     * Model class to represent error summary for display in GOV.UK error summary component
+     */
+    private String fieldId;
+    private String errorMessage;
+    private String anchor;
+
+}
